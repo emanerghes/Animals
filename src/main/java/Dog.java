24bff8de0b828eabc367e94076a854dc5582e216
Emanuela Nerghes <eanerghes@gmail.com>
2023-05-22 21:11:49 +0300

    Cat added

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Dog {
+public class Dog extends Animal {
+    private String breed;
+
+    public Dog(String nume, String sound, String breed) {
+        super(nume, sound);
+        this.breed = breed;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
 }
